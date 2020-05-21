eeaa9a14b523a007e533269c797b7065db952afb
intael <carlosvalde9@gmail.com>
2020-05-21 17:37:59 +0300

    Black formatting

--- src/arguments/validators/argument_validator_factory.py
+++ src/arguments/validators/argument_validator_factory.py
@@ -8,5 +8,7 @@ class ArgumentValidatorFactory:
     ARGUMENT_VALIDATORS = {AssetPeriodValuationArgumentValidator}
 
     @staticmethod
-    def build_asset_valuation_argument_validator(datetime_format: str) -> ArgumentValidator:
+    def build_asset_valuation_argument_validator(
+        datetime_format: str,
+    ) -> ArgumentValidator:
         return AssetPeriodValuationArgumentValidator(datetime_format)
--- src/handlers/token_matching.py
+++ src/handlers/token_matching.py
@@ -6,7 +6,9 @@ VALUE_BY_TOKEN_COMMAND_REGEX_TOKEN = "[$]([a-zA-Z]+[:][0-9]{8}-[0-9]{8})"
 
 
 def match_token(message_word: str) -> Union[str, None]:
-    period_asset_valuation = re.findall(VALUE_BY_TOKEN_COMMAND_REGEX_TOKEN, message_word)
+    period_asset_valuation = re.findall(
+        VALUE_BY_TOKEN_COMMAND_REGEX_TOKEN, message_word
+    )
     if len(period_asset_valuation) > 0:
         return period_asset_valuation[0]  # Only 1 token per word is accepted
     todays_asset_valuation = re.findall(VALUE_COMMAND_REGEX_TOKEN, message_word)
